1a3911d8f93d95ddbf65b8a444c0bef934176b2f
Ulissesortega <Ulisses.ortega@hotmail.com>
2023-02-03 00:24:56 -0800

    update

--- src/app.js
+++ src/app.js
@@ -5,15 +5,19 @@ let image1 = document.getElementById("image1")
 let image2 = document.getElementById("image2")
 let pokename = document.getElementById("pokename")
 let image3 = document.getElementById("image3")
+let pokelocation = document.getElementById("pokelocation")
 
 let pokemoninfo;
 let pokemoninfoLoc;
 
-btn1.addEventListener("click", function(){
+btn1.addEventListener("click", () => {
     GetPokeData(Pokeinput.value);
+    if (Pokeinput.value == ""){
+        alert("Please Enter Pokemon Name or Number!")        
+    } 
 });
 
-btn2.addEventListener("click", function(){
+btn2.addEventListener("click", () =>{
     GetPokeData(getRandomInt(649))
 
 })
@@ -23,10 +27,11 @@ async function GetPokeData(Pokeinput){
     const data = await promise.json();
     pokemoninfo = data;
     console.log(pokemoninfo);
-    image1.src = pokemoninfo.sprites.front_default
-    image2.src = pokemoninfo.sprites.front_shiny
-    pokename.innerText = pokemoninfo.name
-    GetPokeLocation(pokemoninfo.location_area_encounters)
+    image1.src = pokemoninfo.sprites.other.dream_world.front_default
+    image2.src = pokemoninfo.sprites.other.home.front_shiny
+
+    pokename.innerText = "Name: " + pokemoninfo.name
+    GetPokeLocation(pokemoninfo.location_area_encounters)    
     // This limist until pokemon 549
     if (pokemoninfo.id > 649) {
         alert("We only have until Gen 5 or Pokemon 649")
@@ -42,12 +47,16 @@ async function GetPokeLocation(url){
     const promise = await fetch(url)
     const data = await promise.json();
     pokemoninfoLoc = data;
+    pokelocation.innerText = "Location: " + pokemoninfoLoc[0].location_area.name
     if (pokemoninfoLoc.lenght == 0){
-        alert("This Pokemon Does Not Have an Area")
+        pokelocation.innerText = "NA"
     }else{
         console.log(pokemoninfoLoc[0].location_area.name); 
     }
 }
+
+
+
   
 
 
